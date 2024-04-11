791607ec11ef6d5c4902b1ea159d1b2d4cf7cf37
Mark Nguyễn <manh9403@gmail.com>
2024-04-11 10:15:42 +0700

    UI

--- Assets/Game/Scripts/UI/LeaderUI.cs
+++ Assets/Game/Scripts/UI/LeaderUI.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class LeaderUI : MonoBehaviour
 {
+    [SerializeField] private Transform _blockContainer;
+    [SerializeField] private ButtonBlock _btnBlockPrefab;
+    //private List<Block> _
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
