d49523b1014fadd6840c8c9a2bdd4ff41589d6a2
Mark Nguyễn <manh9403@gmail.com>
2024-04-23 00:01:37 +0700

    ButtonBlock

--- Assets/Game/Scripts/UIElements/ButtonBlock.cs
+++ Assets/Game/Scripts/UIElements/ButtonBlock.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonBlock : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Button _btn;
+    [SerializeField] private Image _thrownIcon;
     void Start()
     {
-        
+        _btn.onClick.AddListener(ThrowBomb);
+    }
+
+    private void ThrowBomb()
+    {
+        throw new NotImplementedException();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
