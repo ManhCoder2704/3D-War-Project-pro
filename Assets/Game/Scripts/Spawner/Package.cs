dff39e64e2032c28337673a5af6bb5ba421daf83
Khánh <khanhbui1560@gmail.com>
2024-04-25 20:03:27 +0700

    ad

--- Assets/Game/Scripts/Spawner/Package.cs
+++ Assets/Game/Scripts/Spawner/Package.cs
@@ -4,7 +4,21 @@ public class Package : MonoBehaviour
 {
     [SerializeField] private int _id;
     [SerializeField] private float _dropTime;
+    [SerializeField] private int _reward;
 
     internal float DropTime { get => _dropTime; }
     internal int Id { get => _id; set => _id = value; }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 1 << 7) // collect package
+        {
+            OnGetPackage();
+        }
+    }
+
+    private void OnGetPackage()
+    {
+
+    }
 }
